b8c6e8d6e09b1d04a2a61abad7c9303bf8742e95
mubeen090 <144161677+mubeen090@users.noreply.github.com>
2023-09-12 18:12:26 +0530

    index.js

--- .history/src/index_20230814195037.js
+++ .history/src/index_20230814195037.js
@@ -2,11 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import BrowserRouter  from "react-router-dom";
-import Provider from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
 import rootReducer from "./reducer";
-import configureStore from "@reduxjs/toolkit"
-import  Toaster  from "react-hot-toast";
+import {configureStore} from "@reduxjs/toolkit"
+import { Toaster } from "react-hot-toast";
 
 const store = configureStore({
   reducer:rootReducer,
